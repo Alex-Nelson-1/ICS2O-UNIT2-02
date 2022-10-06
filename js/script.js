a97f22fe2ccfe04b2bf8a2d3191521c93079f0c5
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-06 18:40:51 +0000

    Added all Js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Created by: Alex.Nelson
+ * Created on: Oct 2022
+ * This file contains the JavaScript for index.html
+ */
+
+function calculate() {
+  document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (3 * 5) + " cm²</p>"
+  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + " cm</p>"
+}
