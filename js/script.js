d470ad0db7e257abca211ffa859925f269c4cd2d
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2022-10-06 18:43:03 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
  */
 
 function calculate() {
-  document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (3 * 5) + " cm²</p>"
-  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + " cm</p>"
+  document.getElementById("multiply-math").innerHTML =
+    "<p>The area is: " + 3 * 5 + " cm²</p>"
+  document.getElementById("perimeter-math").innerHTML =
+    "<p>The perimeter is: " + 2 * (5 + 3) + " cm</p>"
 }
